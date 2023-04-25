fc01f71eef01c4f20a19ae1dbb6e6db438aee30e
Tom <454147@student.fontys.nl>
2023-04-25 18:44:52 +0200

    Switched to Entity Framework Core in preparation for hosting

---
+++ SwipperBackup/Data/DatabaseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SwipperBackup.Models;
+
+namespace SwipperBackup.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Listing> Listings { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
