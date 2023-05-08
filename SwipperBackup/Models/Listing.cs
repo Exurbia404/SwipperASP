c851097584cee7180871df20ec6d72c6264f9101
Tom <454147@student.fontys.nl>
2023-05-08 10:55:40 +0200

    Changes to appsettings.json Changes to User.cs and Listing.cs

--- SwipperBackup/Models/Listing.cs
+++ SwipperBackup/Models/Listing.cs
@@ -6,13 +6,24 @@
         public string AnimalName { get; set; }
         public string AnimalSpecies { get; set; }
         public string AnimalImageLink { get; set; }
+        public int Price { get; set; }
+        public bool IsMale { get; set; }
+        public string Address { get; set; }
 
-        public Listing(int id, string animalName, string animalSpecies, string animalImageLink)
+        public int Age { get; set; }
+        public System.DateOnly ListingDate { get; set; }
+
+        public Listing(int id, string animalName, string animalSpecies, string animalImageLink, int price, bool isMale, string address, DateOnly listingDate, int age)
         {
             Id = id;
             AnimalName = animalName;
             AnimalSpecies = animalSpecies;
             AnimalImageLink = animalImageLink;
+            Price = price;
+            IsMale = isMale;
+            Address = address;
+            ListingDate = listingDate;
+            Age = age;
         }
     }
 }
--- SwipperBackup/Startup.cs
+++ SwipperBackup/Startup.cs
@@ -2,7 +2,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using SwipperBackup.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace SwipperBackup;
 public class Startup
@@ -16,21 +15,7 @@ public class Startup
 
     public void ConfigureServices(IServiceCollection services)
     {
-        var serverVersion = new MySqlServerVersion(new Version(8, 0, 29));
-
-        // Configure DbContext using Pomelo.EntityFrameworkCore.MySql
-        services.AddDbContext<DatabaseContext>(options =>
-            options.UseMySql(_configuration.GetConnectionString("DefaultConnection"), serverVersion));
-        services.AddCors(options =>
-        {
-            options.AddPolicy(name: "_myAllowSpecificOrigins",
-                policy =>
-                {
-                    policy.WithOrigins("https://localhost:5002")
-                        .AllowAnyHeader()
-                        .AllowAnyMethod();
-                });
-        });
+        services.AddScoped<DatabaseContext>();
 
         services.AddControllers();
 
@@ -40,12 +25,10 @@ public class Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
-        if (env.IsDevelopment())
-        {
-            app.UseDeveloperExceptionPage();
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
+        app.UseDeveloperExceptionPage();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
 
         app.UseHttpsRedirection();
 
